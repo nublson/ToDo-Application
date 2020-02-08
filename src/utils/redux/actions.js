0b82a5549d4a76924e449633fcbdf315e4cacacd
nubelsondev <nubelsondev@hotmail.com>
2020-02-08 13:12:14 +0000

    fix(actions): fix actions return

--- src/utils/redux/actions.js
+++ src/utils/redux/actions.js
@@ -1,19 +1,25 @@
 import uuid from 'uuid'
 
-export const addTodo = (content = '') => ({
-	type: 'ADD_TODO',
-	todo: {
-		id: uuid(),
-		content,
-		amount: 1
+export const addTodo = (content = '') => {
+	return {
+		type: 'ADD_TODO',
+		todo: {
+			id: uuid(),
+			content,
+			amount: 1
+		}
 	}
-})
+}
 
-export const removeTodo = id => ({
-	type: 'REMOVE_TODO',
-	id
-})
+export const removeTodo = id => {
+	return {
+		type: 'REMOVE_TODO',
+		id
+	}
+}
 
-export const removeAll = () => ({
-	type: 'REMOVE_ALL'
-})
+export const removeAll = () => {
+	return {
+		type: 'REMOVE_ALL'
+	}
+}
